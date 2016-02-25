921bfcf01efaf133fc4449a23342efe26f1caa11
Zack Bennis <zackbennis@gmail.com>
2016-02-25 10:21:44 -0500

    Change Screensaver
    Changed screensaver to “Computer Name” and set askForPassword and
    askForPasswordDelay

--- Screen Lock/Screenlock.sh
+++ Screen Lock/Screenlock.sh
@@ -9,14 +9,10 @@ fi
 
 defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist CleanExit "YES"
 defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist PrefsVersion -int 100
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist idleTime -int 600
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist moduleDict -dict moduleName "iLifeSlideshows" path "/System/Library/Frameworks/ScreenSaver.framework/Resources/iLifeSlideshows.saver" type -int 0
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.ScreenSaverPhotoChooser.$macUUID.plist identifier "/Library/Screen Savers/CORP"
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.ScreenSaverPhotoChooser.$macUUID.plist LastViewedPhotoPath "/Library/Screen Savers/CORP"
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.ScreenSaverPhotoChooser.$macUUID.plist SelectedFolderPath "/Library/Screen Savers/CORP"
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.ScreenSaverPhotoChooser.$macUUID.plist SelectedSource -int 4
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.ScreenSaverPhotoChooser.$macUUID.plist ShufflesPhotos -int 1
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.ScreenSaver.iLifeSlideShows.$macUUID styleKey \"ShiftingTiles\"
-defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.ScreenSaver.iLifeSlideShows styleKey \"ShiftingTiles\"
+defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist idleTime -int 840
+defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist moduleDict -dict displayName "Computer Name" moduleName "Computer Name" path "/System/Library/Frameworks/ScreenSaver.framework/Resources/Computer Name.saver" type -int 0
+defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist showClock -bool 1
+defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist askForPassword -bool 1
+defaults write /Users/$curUser/Library/Preferences/ByHost/com.apple.screensaver.$macUUID.plist askForPasswordDelay -int 1
 
 killall cfprefsd
